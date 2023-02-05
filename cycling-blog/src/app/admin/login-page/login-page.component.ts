50be98345296a9aec0b08213fe203b56404e5ebf
Maksym Mishchenko <mishchenkomv@hotmail.com>
2023-02-05 13:08:12 +0200

    Implement admin pages guard

--- cycling-blog/src/app/admin/login-page/login-page.component.ts
+++ cycling-blog/src/app/admin/login-page/login-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { User } from 'src/app/shared/interfaces';
 import { AuthService } from '../shared/services/auth.service';
 
@@ -11,29 +11,37 @@ import { AuthService } from '../shared/services/auth.service';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor(public auth: AuthService, private router: Router) { }
+  constructor(public auth: AuthService, private router: Router, private route: ActivatedRoute) { }
 
   form!: FormGroup;
   submitted = false;
+  message!: string
 
   ngOnInit() {
+
+    this.route.queryParams.subscribe((params: Params) => {
+      if (params['sessionTimedOut']) {
+        this.message = 'Session timed out. Please, log in.'
+      }
+    });
+
     this.form = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)])
-    })
-  }
+    });
+  };
 
   submit() {
     if (this.form.invalid) {
       return;
-    }
+    };
 
     this.submitted = true;
 
     const user: User = {
       email: this.form.value.email,
       password: this.form.value.password,
-    }
+    };
 
     this.auth.login(user).subscribe(() => {
       this.form.reset();
