fc4e43e3037fc04c30b09a4073d6ed1c5aea563b
Maksym Mishchenko <mishchenkomv@hotmail.com>
2023-02-05 20:37:17 +0200

    Implement create post page without editor

--- cycling-blog/src/app/admin/create-page/create-page.component.ts
+++ cycling-blog/src/app/admin/create-page/create-page.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Post } from 'src/app/shared/interfaces';
 
 @Component({
   selector: 'app-create-page',
   templateUrl: './create-page.component.html',
   styleUrls: ['./create-page.component.scss']
 })
-export class CreatePageComponent {
+export class CreatePageComponent implements OnInit {
+  form!: FormGroup
 
+  ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl('', [Validators.required]),
+      text: new FormControl('', [Validators.required]),
+      author: new FormControl('', [Validators.required])
+    });
+  }
+
+  submit() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    const post: Post = {
+      title: this.form.value.title,
+      text: this.form.value.content,
+      author: this.form.value.author,
+      date: new Date()
+    }
+  }
 }
--- cycling-blog/src/app/shared/shared.module.ts
+++ cycling-blog/src/app/shared/shared.module.ts
@@ -2,8 +2,12 @@ import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 
 @NgModule({
-    imports: [HttpClientModule],
-    exports: [HttpClientModule]
+    imports: [
+        HttpClientModule
+    ],
+    exports: [
+        HttpClientModule
+    ]
 })
 
 export class SharedModule {
