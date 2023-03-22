2e746b78d2a794c1fc68a02a54b5a8c0cf4545b9
Maksym Mishchenko <mishchenkomv@hotmail.com>
2023-03-22 22:35:48 +0200

    Implement reading time for each post

--- cycling-blog/src/app/post-page/post-page.component.ts
+++ cycling-blog/src/app/post-page/post-page.component.ts
@@ -13,6 +13,7 @@ import { PostsService } from '../shared/posts.service';
 export class PostPageComponent implements OnInit {
 
   post$!: Observable<Post>;
+  timeRead: string = '';
   constructor(private route: ActivatedRoute,
     private postsService: PostsService,
     private title: Title, private meta: Meta) { }
@@ -21,11 +22,25 @@ export class PostPageComponent implements OnInit {
     this.post$ = this.route.params.pipe(switchMap((params: Params) => {
       return this.postsService.getPostById(params['id'])
     }));
+
     this.post$.subscribe((post) => {
       this.title.setTitle(post.metaTitle);
       this.meta.addTags([{
         name: 'description', content: post.metaDescription
       }]);
     });
+  }
+
+  getReadindTime(postLength: number): string {
+    if (postLength <= 1500) {
+      return '5 minutes';
+    }
+    if (postLength > 1500) {
+      return '7 minutes';
+    }
+    if (postLength > 2000) {
+      return '10 minutes';
+    }
+    return 'not allocated';
   }
 }
