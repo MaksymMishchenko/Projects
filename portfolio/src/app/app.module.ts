341641a27e764f4f7938e3f4ccbac36716a46137
Maksym Mishchenko <mishchenkomv@hotmail.com>
2023-04-03 18:35:24 +0300

    Implement responsive template

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { PortfolioPageComponent } from './portfolio-page/portfolio-page.component';
 import { ContactPageComponent } from './contact-page/contact-page.component';
 import { NavComponent } from './shared/components/nav/nav.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { NavComponent } from './shared/components/nav/nav.component';
     HomePageComponent,
     PortfolioPageComponent,
     ContactPageComponent,
-    NavComponent
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
