259adab564d31a4969151d9c3d5bd0dd519cb641
Maksym Mishchenko <mishchenkomv@hotmail.com>
2023-11-07 15:30:58 +0200

    Create ContactForm model & implement contact form on contact page

---
+++ CarBlogApp/Models/ContactForm.cs
@@ -0,0 +1,10 @@
+﻿namespace CarBlogApp.Models
+{
+    public class ContactForm
+    {
+        public int? UserId { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Message { get; set; }
+    }
+}
