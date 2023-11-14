cf53ca32bfff346d39ace6c70943588abf3399e6
Maksym Mishchenko <mishchenkomv@hotmail.com>
2023-11-14 12:43:46 +0200

    Implement admin part as area

---
+++ CarBlogApp/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using CarBlogApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarBlogApp.Controllers
+{
+    [Area("Admin")]
+    public class AdminController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var posts = await GetAllPosts();
+
+            return View(posts);
+        }
+
+        private async Task<IEnumerable<Post>> GetAllPosts()
+        {
+            IEnumerable<Post> posts = new List<Post>();
+
+            using (var db = new DatabaseContext())
+            {
+                posts = await db.Posts.ToListAsync();
+            }
+
+            return posts;
+        }
+    }
+}
--- CarBlogApp/Program.cs
+++ CarBlogApp/Program.cs
@@ -4,14 +4,13 @@ namespace CarBlogApp
 {
     public class Program
     {
-
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
             //Add services to the container.           
 
-            builder.Services.AddControllersWithViews();            
+            builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
 
@@ -31,8 +30,12 @@ namespace CarBlogApp
             app.UseAuthorization();
 
             app.MapControllerRoute(
-         name: "default",
-         pattern: "{controller=Home}/{action=Index}/{id?}");
+                 name: "default",
+                 pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.MapControllerRoute(
+                 name: "admin",
+                 pattern: "{area:exists}/{controller=Admin}/{action=Index}/{id?}");
 
             app.Run();
 
