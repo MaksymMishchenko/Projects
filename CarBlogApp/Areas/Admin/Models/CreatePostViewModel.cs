f51f6a0dd25a70c5723f7016c9318f046eb1716b
Maksym Mishchenko <mishchenkomv@hotmail.com>
2023-11-16 16:55:33 +0200

    Asynchronously adds a new post to the database using the provided view model

---
+++ CarBlogApp/Areas/Admin/Models/CreatePostViewModel.cs
@@ -0,0 +1,13 @@
+﻿
+
+using CarBlogApp.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CarBlogApp.Areas.Admin.Models
+{
+    public class CreatePostViewModel
+    {
+        public Post? Post { get; set; }
+        public SelectList? Categories { get; set; }
+    }
+}
