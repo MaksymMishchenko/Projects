4c35c18c094734200ccfb8b466cf3abce5f2897e
Maksym Mishchenko <mishchenkomv@hotmail.com>
2024-01-04 17:16:23 +0200

    Implement custom cart model binder

---
+++ SportsStore/SportsStore.WebUI/Binders/CartModelBinder.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Extensions;
+
+namespace SportsStore.WebUI.Binders
+{
+    public class CartModelBinder : IModelBinder
+    {
+        private const string sessionKey = "Cart";
+
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            var cart = bindingContext.ActionContext.HttpContext.Session.Get<Cart>(sessionKey);
+
+            if (cart == null)
+            {
+                cart = new Cart();
+                bindingContext.ActionContext.HttpContext.Session.Set(sessionKey, cart);
+            }
+
+            bindingContext.Result = ModelBindingResult.Success(cart);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ SportsStore/SportsStore.WebUI/Binders/CartModelBinderProvider.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Binders
+{
+    public class CartModelBinderProvider : IModelBinderProvider
+    {
+        public IModelBinder GetBinder(ModelBinderProviderContext context)
+        {
+            if (context.Metadata.ModelType == typeof(Cart))
+            {
+                return new CartModelBinder();
+            }
+
+            return null;
+        }
+    }
+}
