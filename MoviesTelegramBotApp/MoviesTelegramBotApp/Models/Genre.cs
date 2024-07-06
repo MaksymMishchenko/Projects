d9a40941b7fdb0b4535fef86bcd363ea5a60882f
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-07-06 19:45:50 +0300

    Create Genre and Movie models

---
+++ MoviesTelegramBotApp/MoviesTelegramBotApp/Models/Genre.cs
@@ -0,0 +1,10 @@
+﻿namespace MoviesTelegramBotApp.Models
+{
+    internal class Genre
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public ICollection<Movie>? Movies { get; set; }
+    }
+}
---
+++ MoviesTelegramBotApp/MoviesTelegramBotApp/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿namespace MoviesTelegramBotApp.Models
+{
+    internal class Movie
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+
+        public string? Country { get; set; }
+        public decimal? Budget { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? MovieUrl { get; set; }
+        public string? InterestFactsUrl { get; set; }
+        public string? BehindTheScene { get; set; }
+
+        public int GenreId { get; set; }
+        public Genre? Genre { get; set; }
+    }
+}
