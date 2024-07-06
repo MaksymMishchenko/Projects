94759d1b5b4aae51f52619bdfe332cfba9f2d190
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-07-06 19:35:24 +0300

    Add MoviesTelegramBotApp.Tests project

--- MoviesTelegramBotApp/MoviesTelegramBotApp/Program.cs
+++ MoviesTelegramBotApp/MoviesTelegramBotApp/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace MoviesTelegramBotApp
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello AppBot");
+        }
+    }
+}
