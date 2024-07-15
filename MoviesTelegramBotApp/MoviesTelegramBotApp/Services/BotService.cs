32afbbd4bdd1e92a671244d8826ae0db3d66824d
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-07-15 22:26:01 +0300

    Configure host and add Movie and TelegramBot services, UpdateHandler

---
+++ MoviesTelegramBotApp/MoviesTelegramBotApp/Services/BotService.cs
@@ -0,0 +1,47 @@
+﻿using MoviesTelegramBotApp.Interfaces;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InputFiles;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace MoviesTelegramBotApp.Services
+{
+    internal class BotService : IBotService
+    {
+        private TelegramBotClient _botClient;
+        public BotService(TelegramBotClient botClient)
+        {
+            _botClient = botClient;
+        }
+        public async Task<User> GetBotDetailsAsync()
+        {
+            return await _botClient.GetMeAsync();
+        }
+
+        public async Task SendTextMessageAsync(long chatId, string message, ParseMode parseMode, ReplyKeyboardMarkup replyMarkup, CancellationToken cancellationToken)
+        {
+            await _botClient.SendTextMessageAsync(chatId, message, parseMode, replyMarkup: replyMarkup, cancellationToken: cancellationToken);
+        }
+
+        public async Task SendTextMessageAsync(long chatId, string response, ParseMode parseMode)
+        {
+            await _botClient.SendTextMessageAsync(chatId, response, parseMode);
+        }
+
+        public async Task SendTextMessageAsync(long chatId, string response)
+        {
+            await _botClient.SendTextMessageAsync(chatId, response);
+        }
+
+        public async Task SendTextMessageAsync(long chatId, string response, CancellationToken cancellationToken)
+        {
+            await _botClient.SendTextMessageAsync(chatId, response, cancellationToken: cancellationToken);
+        }
+
+        public async Task SendPhotoWithInlineButtonUrlAsync(long chatId, InputOnlineFile photoUrl, string caption, ParseMode parseMode, InlineKeyboardMarkup replyMarkup)
+        {
+            await _botClient.SendPhotoAsync(chatId: chatId, photo: photoUrl, caption: caption, parseMode: parseMode, replyMarkup: replyMarkup);
+        }
+    }
+}
---
+++ MoviesTelegramBotApp/MoviesTelegramBotApp/UpdateHandler.cs
@@ -0,0 +1,101 @@
+﻿using MoviesTelegramBotApp.Interfaces;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+internal class UpdateHandler
+{
+    private readonly IBotService _botService;
+    private readonly IMovieService _movieService;
+
+    public UpdateHandler(IBotService botService, IMovieService movieService)
+    {
+        _botService = botService;
+        _movieService = movieService;
+    }
+
+    public async Task HandleUpdateAsync(ITelegramBotClient bot, Update update, CancellationToken cancellationToken)
+    {
+        if (update.Type == UpdateType.Message && update.Message!.Type == MessageType.Text)
+        {
+            var chatId = update.Message.Chat.Id;
+            var messageText = update.Message.Text;
+
+            var startKeyBoard = new ReplyKeyboardMarkup(new KeyboardButton("Start")) { ResizeKeyboard = true };
+
+            if (messageText?.ToLower() == "/start")
+            {
+                var botDetails = await _botService.GetBotDetailsAsync();
+                var response = $"<strong>Lights, Camera, Action!</strong>\nWelcome to <em><strong>{botDetails.FirstName}</strong></em>, the hottest Telegram hangout for movie lovers! Whether you're a die-hard cinephile or just enjoy a good popcorn flick, this is your spot to discuss all things film.";
+                await _botService.SendTextMessageAsync(chatId, response, ParseMode.Html);
+                await SendMenuAsync(chatId, cancellationToken);
+            }
+            else
+            {
+                await HandleMenuResponseAsync(chatId, messageText!, cancellationToken);
+            }
+        }
+    }
+
+    private async Task SendMenuAsync(long chatId, CancellationToken cts)
+    {
+        var replyKeyBoardMarkup = new ReplyKeyboardMarkup(new[]
+        {
+
+            new KeyboardButton[] { "Movies", "Cartoons"}
+
+            })
+        {
+            ResizeKeyboard = true
+        };
+
+        await _botService.SendTextMessageAsync(
+            chatId,
+            "Choose an option",
+            parseMode: ParseMode.Html,
+            replyKeyBoardMarkup,
+            cancellationToken: cts);
+    }
+
+    private async Task HandleMenuResponseAsync(long chatId, string messageText, CancellationToken cancellationToken)
+    {
+        switch (messageText)
+        {
+            case "Movies":
+                await SendMoviesAsync(chatId, cancellationToken);
+                break;
+            case "Cartoons":
+                //todo: functionality in progress
+                break;
+
+            default:
+                await _botService.SendTextMessageAsync(chatId, "The command is not recognized");
+                break;
+        }
+    }
+
+    private async Task SendMoviesAsync(long chatId, CancellationToken cancellationToken)
+    {
+        var movies = _movieService.GetMovies();
+        var response = _movieService.BuildMoviesResponse(movies);
+
+        foreach (var movie in movies)
+        {
+            if (!string.IsNullOrEmpty(movie.ImageUrl))
+            {
+                await _botService.SendPhotoWithInlineButtonUrlAsync(
+                    chatId,
+                    photoUrl: new Telegram.Bot.Types.InputFiles.InputOnlineFile(movie.ImageUrl),
+                    caption: $"<strong>Name:</strong> {movie.Title}\n<strong>Genre:</strong> {movie.Genre}\n<strong>Description:</strong> {movie.Description}\n<strong>Country:</strong> {movie.Country}\n<strong>Budget:</strong> {movie.Budget}\n<strong>Trailer:</strong>",
+                    parseMode: ParseMode.Html,
+                    replyMarkup: new InlineKeyboardMarkup(
+            InlineKeyboardButton.WithUrl("Check out the trailer", movie.MovieUrl)));
+            }
+            else
+            {
+                await _botService.SendTextMessageAsync(chatId, response, cancellationToken);
+            }
+        }
+    }
+}
