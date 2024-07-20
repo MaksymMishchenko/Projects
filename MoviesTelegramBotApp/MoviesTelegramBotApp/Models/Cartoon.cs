ee3cb0261cdba354f5521ef8ee0ebe50f9c078de
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-07-20 19:59:57 +0300

    Create Cartoon and Cartoon Genre model

---
+++ MoviesTelegramBotApp/MoviesTelegramBotApp/Models/Cartoon.cs
@@ -0,0 +1,15 @@
+﻿namespace MoviesTelegramBotApp.Models
+{
+    internal class Cartoon
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Budget { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? CartoonUrl { get; set; }
+
+        public int CartoonGenreId { get; set; }
+        public CartoonGenre? Genre { get; set; }
+    }
+}
--- MoviesTelegramBotApp/MoviesTelegramBotApp/Models/Movie.cs
+++ MoviesTelegramBotApp/MoviesTelegramBotApp/Models/Movie.cs
@@ -5,7 +5,6 @@
         public int Id { get; set; }
         public string? Title { get; set; }
         public string? Description { get; set; }
-
         public string? Country { get; set; }
         public string? Budget { get; set; }
         public string? ImageUrl { get; set; }
