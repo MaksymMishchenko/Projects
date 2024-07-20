ba3873eb1fec4cd414e154b1bd480848530584fa
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-07-20 19:20:41 +0300

    Implement prevoius-next movies navigation

--- MoviesTelegramBotApp/MoviesTelegramBotApp/Interfaces/IMovieService.cs
+++ MoviesTelegramBotApp/MoviesTelegramBotApp/Interfaces/IMovieService.cs
@@ -4,6 +4,7 @@ namespace MoviesTelegramBotApp.Interfaces
 {
     public interface IMovieService
     {
+        Task<int> Count { get; }
         Task<List<Movie>> GetAllMoviesAsync(int moviePage = 1);
         string BuildMoviesResponse(List<Movie> movies);
     }
