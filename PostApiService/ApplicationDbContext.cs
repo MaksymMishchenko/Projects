3ce3cd6e835ec491c927de28797a60ec5b81ce15
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-10-06 20:57:22 +0300

    Seed db with test data

--- PostApiService/ApplicationDbContext.cs
+++ PostApiService/ApplicationDbContext.cs
@@ -1,26 +1,64 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using PostApiService.Models;
 
-namespace PostApiService
+public class ApplicationDbContext : DbContext
 {
-    public class ApplicationDbContext : DbContext
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
     {
-        public DbSet<Post> Posts { get; set; }
-        public DbSet<Comment> Comments { get; set; }
-       
-
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            // Configure Post-Comment relationship (optional)
-            modelBuilder.Entity<Post>()
-                .HasMany(p => p.Comments)
-                .WithOne(c => c.Post)
-                .HasForeignKey(c => c.PostId);
-
-            base.OnModelCreating(modelBuilder);
-        }
     }
 
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Seed posts
+        modelBuilder.Entity<Post>().HasData(
+            new Post
+            {
+                PostId = 1,
+                Title = "First Post",
+                Content = "This is the content of the first post.",
+                CreateAt = DateTime.Now
+            },
+            new Post
+            {
+                PostId = 2,
+                Title = "Second Post",
+                Content = "This is the content of the second post.",
+                CreateAt = DateTime.Now
+            }
+        );
+
+        // Seed comments for the posts
+        modelBuilder.Entity<Comment>().HasData(
+            new Comment
+            {
+                CommentId = 1,
+                Author = "John Doe",
+                Content = "Great post!",
+                CreatedAt = DateTime.Now,
+                PostId = 1 // Link this comment to PostId 1
+            },
+            new Comment
+            {
+                CommentId = 2,
+                Author = "Jane Doe",
+                Content = "I totally agree with this!",
+                CreatedAt = DateTime.Now,
+                PostId = 1 // Link this comment to PostId 1
+            },
+            new Comment
+            {
+                CommentId = 3,
+                Author = "Alice",
+                Content = "This is a comment on the second post.",
+                CreatedAt = DateTime.Now,
+                PostId = 2 // Link this comment to PostId 2
+            }
+        );
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
