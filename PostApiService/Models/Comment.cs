263390f6089ba5db3f9b23145d7228884e874fea
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-10-06 17:34:03 +0300

    Create Post and Comment models

---
+++ PostApiService/Models/Comment.cs
@@ -0,0 +1,13 @@
+﻿namespace PostApiService.Models
+{
+    public class Comment
+    {
+        public int CommentId { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public int PostId { get; set; }
+        public Post Post { get; set; }
+    }
+}
