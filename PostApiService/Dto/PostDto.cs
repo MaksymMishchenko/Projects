1806bc1fe2f82de05f95d9a9540627082715e43f
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-10-06 22:31:08 +0300

    Implement logic getting posts and comments related to specific post

---
+++ PostApiService/Dto/PostDto.cs
@@ -0,0 +1,19 @@
+﻿namespace PostApiService.Dto
+{
+    public class PostDto
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreateAt { get; set; }
+        public List<CommentDto> Comments { get; set; }
+    }
+
+    public class CommentDto
+    {
+        public int CommentId { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
+        public DateTime CreateAt { get; set; }        
+    }
+}
