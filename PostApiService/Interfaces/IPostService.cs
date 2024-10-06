c0861c999399118825de92f0584266170ee8aadb
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-10-06 21:30:02 +0300

    Implement PostsService and PostController endpoints

---
+++ PostApiService/Interfaces/IPostService.cs
@@ -0,0 +1,12 @@
+﻿using PostApiService.Models;
+
+namespace PostApiService.Interfaces
+{
+    public interface IPostService
+    {
+        Task<List<Post>> GetAllPosts();
+        Task AddPostAsync(Post post);
+        Task EditPostAsync(Post post);
+        Task DeletePostAsync(int postId);
+    }
+}
