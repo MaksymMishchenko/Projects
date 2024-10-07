6b4f63a046fb46334be5784b34ae5005ad9b97cd
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-10-07 15:29:45 +0300

    Implement routing and rendering children routes related to main-layout

--- cooking-blog/src/app/app.routes.ts
+++ cooking-blog/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PostPageComponent } from './post-page/post-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: MainLayoutComponent, children: [
+        {path: '', component: HomePageComponent},
+        {path: 'post/:id', component: PostPageComponent}
+    ]}
+];
