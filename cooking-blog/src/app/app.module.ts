3f0023448e64d049de22cc80a4042732f6cf50ca
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-10-07 15:29:45 +0300

    Implement routing and rendering children routes related to main-layout

--- cooking-blog/src/app/app.module.ts
+++ cooking-blog/src/app/app.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [
+        AppComponent,
+        MainLayoutComponent
+    ],
     imports: [
         BrowserModule,
-        FormsModule],
+        FormsModule,
+        AppRoutingModule
+    ],
     providers: [],
     bootstrap: [AppComponent]
 })
