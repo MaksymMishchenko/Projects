df45f21c454113608b7c6e3d4758e74425274a22
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-10-24 16:43:42 +0300

    Cover AddCommentAsync method in CommentService with integration tests

---
+++ PostApiService.Tests/IntegrationTests/CommentServiceIntegrationTests.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PostApiService.Models;
+using PostApiService.Services;
+
+namespace PostApiService.Tests.IntegrationTests
+{
+    public class CommentServiceIntegrationTests : IClassFixture<IntegrationTestFixture>
+    {
+        private readonly ApplicationDbContext _context;
+        private PostService _postService;
+        private CommentService _commentService;
+        public CommentServiceIntegrationTests(IntegrationTestFixture fixture)
+        {
+            _context = fixture.Context;
+            _postService = new PostService(_context);
+            _commentService = new CommentService(_context);
+        }
+
+        private async Task SeedTestData()
+        {
+            var post1 = new Post
+            {
+                Title = "Test Post",
+                Description = "This is a test post.",
+                Content = "Content of the test post.",
+                ImageUrl = "http://example.com/image.jpg",
+                MetaTitle = "Test Post Meta Title",
+                MetaDescription = "Test Post Meta Description",
+                Slug = "test-post-one",
+                Comments = new List<Comment>()
+            };
+
+            var post2 = new Post
+            {
+                Title = "Test Post 2",
+                Description = "This is a test post 2.",
+                Content = "Content of the test post 2.",
+                ImageUrl = "http://example.com/image2.jpg",
+                MetaTitle = "Test Post Meta Title 2",
+                MetaDescription = "Test Post Meta Description 2",
+                Slug = "test-post-two",
+                Comments = new List<Comment>()
+            };
+
+            await _postService.AddPostAsync(post1);
+            await _postService.AddPostAsync(post2);
+        }
+
+        [Fact]
+        public async Task AddCommentAsync_Should_Add_Comment_To_Specific_Post()
+        {
+            // Arrange
+            await SeedTestData();
+
+            var post = await _context.Posts.FirstOrDefaultAsync(p => p.Slug == "test-post-one");
+            Assert.NotNull(post);
+
+            var postId = post.PostId;
+
+            var comment = new Comment
+            {
+                Content = "Comment content for post 1",
+                Author = "Bob",
+                CreatedAt = DateTime.Now,
+            };
+
+            // Act
+            await _commentService.AddCommentAsync(postId, comment);
+
+            // Assert
+            var updatedPost = await _context.Posts
+                .Include(p => p.Comments)
+                .FirstOrDefaultAsync(p => p.PostId == postId);
+            Assert.NotNull(post);
+            Assert.Contains(updatedPost.Comments, c => c.Content == comment.Content && c.Author == comment.Author);
+            Assert.Single(updatedPost.Comments);
+        }
+    }
+}
