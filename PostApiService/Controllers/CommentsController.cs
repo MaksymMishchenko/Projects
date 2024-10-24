cc78964ab5ca7a51634f61725821aeb07d9aaa8a
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-10-24 18:15:04 +0300

    Split posts and comments controllers logic seperately

---
+++ PostApiService/Controllers/CommentsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PostApiService.Interfaces;
+using PostApiService.Models;
+
+namespace PostApiService.Controllers
+{
+    [Controller]
+    [Route("api/[controller]")]
+    public class CommentsController : Controller
+    {
+        private readonly ICommentService _commentService;
+        public CommentsController(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+
+        [HttpPost("posts/{postId}")]
+        public async Task<IActionResult> AddComment(int postId, [FromBody] Comment comment)
+        {
+            await _commentService.AddCommentAsync(postId, comment);
+            return Ok();
+        }
+
+        [HttpPut("{commentId}")]
+        public async Task<IActionResult> EditComment(int commentId, [FromBody] Comment comment)
+        {
+            if (commentId != comment.CommentId)
+            {
+                return BadRequest();
+            }
+
+            await _commentService.EditCommentAsync(comment);
+            return Ok();
+        }
+
+        [HttpDelete("{commentId}")]
+        public async Task<IActionResult> DeleteComment(int commentId)
+        {
+            await _commentService.DeleteCommentAsync(commentId);
+            return Ok();
+        }
+    }
+}
