d98c1d8782bbac1976da129a9d49a7691ee215c6
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-10-27 23:08:46 +0200

    Cover UpdateComment method in CommentsController with E2ETests

--- PostApiService/PostApiService.Tests/E2ETests/CommentsControllerE2ETests.cs
+++ PostApiService/PostApiService.Tests/E2ETests/CommentsControllerE2ETests.cs
@@ -86,6 +86,70 @@ namespace PostApiService.Tests.E2Tests
                 Assert.NotNull(addedComment);
                 Assert.Equal(newComment.Content, addedComment.Content);
                 Assert.Equal(newComment.Author, addedComment.Author);
+                await context.Database.EnsureDeletedAsync();
+            }
+        }
+
+        [Fact]
+        public async Task EditComment_ShoulEditComment_IfExists()
+        {
+            // Arrange
+            var factory = CreateFactory();
+            var client = factory.CreateClient();
+
+            int postId = 1;
+
+            var post = CreateTestPost(
+                "Post title",
+                "Post content",
+                "Post author",
+                "image.jpg",
+                "Post description",
+                "post-slug",
+                "Meta title",
+                "Meta description"
+            );
+
+            await SeedPostAsync(post, factory);
+
+            var initialComment = new Comment
+            {
+                Content = "This is a test comment.",
+                Author = "Test Author"
+            };
+
+            var jsonContent = JsonSerializer.Serialize(initialComment);
+            var httpContent = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+            var createResponse = await client.PostAsync($"/api/comments/posts/{postId}", httpContent);
+            Assert.Equal(HttpStatusCode.OK, createResponse.StatusCode);
+
+            int commentId = 1;
+
+            var updatedComment = new Comment
+            {
+                CommentId = commentId,
+                Content = "Updated content"
+            };
+
+            var jsonUpdatedContent = JsonSerializer.Serialize(updatedComment);
+            var httpUpdatedContent = new StringContent(jsonUpdatedContent, Encoding.UTF8, "application/json");
+
+            // Act
+            var response = await client.PutAsync($"/api/comments/{commentId}", httpUpdatedContent);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            using (var scope = factory.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                var editedComment = await context.Comments
+                    .FirstOrDefaultAsync(c => c.Content == "Updated content"
+                    && c.Author == "Test Author");
+                Assert.NotNull(editedComment);
+                Assert.Equal(updatedComment.Content, editedComment.Content);
+                await context.Database.EnsureDeletedAsync();
             }
         }
 
@@ -97,7 +161,8 @@ namespace PostApiService.Tests.E2Tests
             string description,
             string slug,
             string metaTitle,
-            string metaDescription)
+            string metaDescription
+            )
         {
             return new Post
             {
