bfcb58c747f2f45ab142158b8fe406976857ec91
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-10-28 20:47:43 +0200

    Create TestFactoryFixture for E2ETests and implement it for CommentsControllerE2ETests

--- PostApiService/PostApiService.Tests/E2ETests/CommentsControllerE2ETests.cs
+++ PostApiService/PostApiService.Tests/E2ETests/CommentsControllerE2ETests.cs
@@ -1,8 +1,7 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using PostApiService.Models;
+using PostApiService.Tests.E2ETests;
 using System.Net;
 using System.Net.Http.Json;
 using System.Text;
@@ -10,44 +9,43 @@ using System.Text.Json;
 
 namespace PostApiService.Tests.E2Tests
 {
-    public class CommentsControllerE2ETests : IClassFixture<WebApplicationFactory<Program>>
+    public class CommentsControllerE2ETests : IClassFixture<TestFactoryFixture>
     {
-        private HttpClient _client;
-        private readonly WebApplicationFactory<Program> _factory;
+        private readonly TestFactoryFixture _fixture;
+        private readonly HttpClient _client;
 
-        public CommentsControllerE2ETests(WebApplicationFactory<Program> factory)
+        public CommentsControllerE2ETests(TestFactoryFixture fixture)
         {
-            _factory = factory;
+            _fixture = fixture;
+            _client = fixture.Client;
 
         }
-        private WebApplicationFactory<Program> CreateFactory()
-        {
-            return new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
-            {
-                builder.UseEnvironment("Test");
-                builder.ConfigureServices(services =>
-                {
-                    var dbDescriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<ApplicationDbContext>));
+        //private WebApplicationFactory<Program> CreateFactory()
+        //{
+        //    return new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
+        //    {
+        //        builder.UseEnvironment("Test");
+        //        builder.ConfigureServices(services =>
+        //        {
+        //            var dbDescriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<ApplicationDbContext>));
 
-                    if (dbDescriptor != null)
-                    {
-                        services.Remove(dbDescriptor);
-                    }
+        //            if (dbDescriptor != null)
+        //            {
+        //                services.Remove(dbDescriptor);
+        //            }
 
-                    services.AddDbContext<ApplicationDbContext>(options =>
-                    {
-                        options.UseInMemoryDatabase("TestDb");
-                    });
-                });
-            });
-        }
+        //            services.AddDbContext<ApplicationDbContext>(options =>
+        //            {
+        //                options.UseInMemoryDatabase("TestDb");
+        //            });
+        //        });
+        //    });
+        //}
 
         [Fact]
         public async Task AddComment_ReturnsOk_WhenCommentIsAddedSuccessfully()
         {
-            // Arrange
-            var factory = CreateFactory();
-            var client = factory.CreateClient();
+            // Arrange           
 
             int postId = 1;
             var post = CreateTestPost(
@@ -61,7 +59,7 @@ namespace PostApiService.Tests.E2Tests
                 "Meta description"
             );
 
-            await SeedPostAsync(post, factory);
+            await SeedPostAsync(post);
 
             var newComment = new Comment
             {
@@ -73,31 +71,25 @@ namespace PostApiService.Tests.E2Tests
             var httpContent = new StringContent(jsonContent, Encoding.UTF8, "application/json");
 
             // Act
-            var response = await client.PostAsync($"/api/comments/posts/{postId}", httpContent);
+            var response = await _client.PostAsync($"/api/comments/posts/{postId}", httpContent);
 
             // Assert
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
 
-            using (var scope = factory.Services.CreateScope())
-            {
-                var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-                var addedComment = await context.Comments
-                    .FirstOrDefaultAsync(c => c.Content == newComment.Content && c.PostId == postId);
+            using var scope = _fixture.Factory.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            var addedComment = await context.Comments
+                .FirstOrDefaultAsync(c => c.Content == newComment.Content && c.PostId == postId);
 
-                Assert.NotNull(addedComment);
-                Assert.Equal(newComment.Content, addedComment.Content);
-                Assert.Equal(newComment.Author, addedComment.Author);
-                await context.Database.EnsureDeletedAsync();
-            }
+            Assert.NotNull(addedComment);
+            Assert.Equal(newComment.Content, addedComment.Content);
+            Assert.Equal(newComment.Author, addedComment.Author);
         }
 
         [Fact]
         public async Task EditComment_ShoulEditComment_IfExists()
         {
-            // Arrange
-            var factory = CreateFactory();
-            var client = factory.CreateClient();
-
+            // Arrange           
             int postId = 1;
 
             var post = CreateTestPost(
@@ -111,7 +103,7 @@ namespace PostApiService.Tests.E2Tests
                 "Meta description"
             );
 
-            await SeedPostAsync(post, factory);
+            await SeedPostAsync(post);
 
             var initialComment = new Comment
             {
@@ -122,7 +114,7 @@ namespace PostApiService.Tests.E2Tests
             var jsonContent = JsonSerializer.Serialize(initialComment);
             var httpContent = new StringContent(jsonContent, Encoding.UTF8, "application/json");
 
-            var createResponse = await client.PostAsync($"/api/comments/posts/{postId}", httpContent);
+            var createResponse = await _client.PostAsync($"/api/comments/posts/{postId}", httpContent);
             Assert.Equal(HttpStatusCode.OK, createResponse.StatusCode);
 
             int commentId = 1;
@@ -137,30 +129,23 @@ namespace PostApiService.Tests.E2Tests
             var httpUpdatedContent = new StringContent(jsonUpdatedContent, Encoding.UTF8, "application/json");
 
             // Act
-            var response = await client.PutAsync($"/api/comments/{commentId}", httpUpdatedContent);
+            var response = await _client.PutAsync($"/api/comments/{commentId}", httpUpdatedContent);
 
             // Assert
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
 
-            using (var scope = factory.Services.CreateScope())
-            {
-                var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-                var editedComment = await context.Comments
-                    .FirstOrDefaultAsync(c => c.Content == "Updated content"
-                    && c.Author == "Test Author");
-                Assert.NotNull(editedComment);
-                Assert.Equal(updatedComment.Content, editedComment.Content);
-                await context.Database.EnsureDeletedAsync();
-            }
+            using var scope = _fixture.Factory.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            var editedComment = await context.Comments.FindAsync(commentId);
+
+            Assert.NotNull(editedComment);
+            Assert.Equal(updatedComment.Content, editedComment.Content);
         }
 
         [Fact]
         public async Task DeleteComment_ShoulRemoveCommentById()
         {
-            // Arrange
-            var factory = CreateFactory();
-            var client = factory.CreateClient();
-
+            // Arrange                        
             var postId = 1;
 
             var initialPost = CreateTestPost(
@@ -174,7 +159,7 @@ namespace PostApiService.Tests.E2Tests
                "Meta description"
                 );
 
-            await SeedPostAsync(initialPost, factory);
+            await SeedPostAsync(initialPost);
 
             var initialComment = new Comment
             {
@@ -183,21 +168,18 @@ namespace PostApiService.Tests.E2Tests
                 PostId = postId
             };
 
-            var commentResponse = await client.PostAsJsonAsync($"/api/comments/posts/{postId}", initialComment);
+            var commentResponse = await _client.PostAsJsonAsync($"/api/comments/posts/{postId}", initialComment);
             Assert.Equal(HttpStatusCode.OK, commentResponse.StatusCode);
 
             // Act
-            var response = await client.DeleteAsync($"/api/comments/{initialComment.CommentId}");
+            var response = await _client.DeleteAsync($"/api/comments/{initialComment.CommentId}");
 
             // Assert
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            using (var scope = factory.Services.CreateScope())
-            {
-                var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-                var removedPost = await context.Comments.FindAsync(initialComment.CommentId);
-                Assert.Null(removedPost);
-                await context.Database.EnsureDeletedAsync();
-            }
+            using var scope = _fixture.Factory.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            var removedPost = await context.Comments.FindAsync(initialComment.CommentId);
+            Assert.Null(removedPost);
         }
 
         private Post CreateTestPost(
@@ -225,14 +207,12 @@ namespace PostApiService.Tests.E2Tests
             };
         }
 
-        private async Task SeedPostAsync(Post post, WebApplicationFactory<Program> factory)
+        private async Task SeedPostAsync(Post post)
         {
-            using (var scope = factory.Services.CreateScope())
-            {
-                var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-                context.Posts.Add(post);
-                await context.SaveChangesAsync();
-            }
+            using var scope = _fixture.Factory.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            context.Posts.Add(post);
+            await context.SaveChangesAsync();
         }
     }
 }
---
+++ PostApiService/PostApiService.Tests/E2ETests/TestFactoryFixture.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PostApiService.Tests.E2ETests
+{
+    public class TestFactoryFixture : IAsyncLifetime
+    {
+        public WebApplicationFactory<Program> Factory { get; private set; }
+        public HttpClient Client => Factory.CreateClient();
+
+        public async Task InitializeAsync()
+        {
+            Factory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
+            {
+                builder.UseEnvironment("Test");
+                builder.ConfigureServices(services =>
+                {
+                    var appDbContextDescriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<ApplicationDbContext>));
+
+                    if (appDbContextDescriptor != null)
+                    {
+                        services.Remove(appDbContextDescriptor);
+                    }
+
+                    services.AddDbContext<ApplicationDbContext>(options =>
+                    {
+                        options.UseInMemoryDatabase("TestDb");
+                    });
+                });
+            });
+
+            using var scope = Factory.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            await context.Database.EnsureCreatedAsync();
+        }
+
+        public async Task DisposeAsync()
+        {
+            using var scope = Factory.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            await context.Database.EnsureDeletedAsync();
+        }
+    }
+}
