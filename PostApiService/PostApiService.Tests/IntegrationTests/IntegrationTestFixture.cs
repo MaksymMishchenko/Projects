56c224818311a5288897162a985d4838fa3ff481
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-10-28 23:48:52 +0200

    Refactor IntegrationTestFixture and PostServiceIntegrationTests

--- PostApiService/PostApiService.Tests/IntegrationTests/IntegrationTestFixture.cs
+++ PostApiService/PostApiService.Tests/IntegrationTests/IntegrationTestFixture.cs
@@ -1,20 +1,24 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using PostApiService.Services;
 
 namespace PostApiService.Tests.IntegrationTests
 {
-    public class IntegrationTestFixture
+    public class IntegrationTestFixture : IDisposable
     {
-        public ApplicationDbContext CreateContext()
+        public ApplicationDbContext Context { get; private set; }
+        public PostService PostService { get; private set; }
+        public IntegrationTestFixture()
         {
-
             var options = new DbContextOptionsBuilder<ApplicationDbContext>()
                 .UseInMemoryDatabase(Guid.NewGuid().ToString())
                 .Options;
+            Context = new ApplicationDbContext(options);
+            PostService = new PostService(Context);
+        }
 
-            var context = new ApplicationDbContext(options);
-            context.Database.EnsureCreated();
-
-            return context;
+        public void Dispose()
+        {
+            Context.Dispose();
         }
     }
 }
