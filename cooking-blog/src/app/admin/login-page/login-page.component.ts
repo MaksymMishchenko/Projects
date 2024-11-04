486ef2cd3874af45a6625a6c02008f7b93f9ec5e
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-11-04 17:13:37 +0200

    Implement login form validation

--- cooking-blog/src/app/admin/login-page/login-page.component.ts
+++ cooking-blog/src/app/admin/login-page/login-page.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit {
+  form: FormGroup
 
+  ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)])
+    })
+  }
+
+  submit() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    
+  }
 }
