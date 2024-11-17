055a1547ae51188093ac74d1316b4504dc56cc0f
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-11-17 22:39:35 +0200

    Cover DeleteMethodAsync with unit tests

---
+++ PostApiService/PostApiService.Tests/Fixtures/InMemoryDatabaseFixture.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PostApiService.Tests.Fixtures
+{
+    public class InMemoryDatabaseFixture : IAsyncLifetime
+    {
+        public ApplicationDbContext GetContext { get; private set; }
+
+        public InMemoryDatabaseFixture()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            GetContext = new ApplicationDbContext(options);
+        }
+
+        public async Task DisposeAsync()
+        {
+            await GetContext.Database.EnsureDeletedAsync();
+        }
+
+        public async Task InitializeAsync()
+        {
+            await GetContext.Database.EnsureCreatedAsync();
+        }
+    }
+}
--- PostApiService/PostApiService.Tests/UnitTests/CommentServiceTests.cs
+++ PostApiService/PostApiService.Tests/UnitTests/CommentServiceTests.cs
@@ -2,33 +2,30 @@
 using Microsoft.Extensions.Logging;
 using PostApiService.Models;
 using PostApiService.Services;
+using PostApiService.Tests.Fixtures;
 
 namespace PostApiService.Tests.UnitTests
 {
-    public class CommentServiceTests
+    public class CommentServiceTests : IClassFixture<InMemoryDatabaseFixture>
     {
-        private (CommentService commentService, ApplicationDbContext context) GetCommentService()
+        private ApplicationDbContext _dbContext;
+        private CommentService _commentService;
+        private ILogger<CommentService> _logger;
+
+        public CommentServiceTests(InMemoryDatabaseFixture fixture)
         {
-            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-                .UseInMemoryDatabase(Guid.NewGuid().ToString())
-                .Options;
-
-            var dbContext = new ApplicationDbContext(options);
-            var logger = new LoggerFactory().CreateLogger<CommentService>();
-            var commentService = new CommentService(dbContext, logger);
-
-            return (commentService, dbContext);
+            _dbContext = fixture.GetContext;
+            _logger = new LoggerFactory().CreateLogger<CommentService>();
+            _commentService = new CommentService(_dbContext, _logger);
         }
 
         [Fact]
         public async Task AddCommentAsync_ShouldReturnTrue_WhenPostExists()
         {
-            // Arrange
-            var (commentService, context) = GetCommentService();
-
+            // Arrange            
             var postId = 1;
 
-            context.Posts.Add(new Post
+            _dbContext.Posts.Add(new Post
             {
                 PostId = postId,
                 Title = "Test post",
@@ -39,18 +36,18 @@ namespace PostApiService.Tests.UnitTests
                 MetaTitle = "Test meta title",
                 MetaDescription = "Tets meta descr"
             });
-            await context.SaveChangesAsync();
+            await _dbContext.SaveChangesAsync();
 
             var comment = new Comment { Content = "Some test comment", Author = "Test author" };
 
             // Act
-            var result = await commentService.AddCommentAsync(postId, comment);
+            var result = await _commentService.AddCommentAsync(postId, comment);
 
             // Assert
             Assert.True(result);
-            Assert.Single(await context.Comments.ToListAsync());
+            Assert.Single(await _dbContext.Comments.ToListAsync());
 
-            var addedComment = await context.Comments
+            var addedComment = await _dbContext.Comments
                 .FirstOrDefaultAsync(c => c.PostId == postId && c.Content == "Some test comment");
             Assert.NotNull(addedComment);
             Assert.Equal("Some test comment", addedComment.Content);
@@ -60,85 +57,42 @@ namespace PostApiService.Tests.UnitTests
         [Fact]
         public async Task AddCommentAsync_ShouldThrowKeyNotFoundException_WhenPostDoesNotExist()
         {
-            // Arrange
-            var (commentService, context) = GetCommentService();
-
+            // Arrange                       
             var postId = 1;
-            var comment = new Comment { Content = "Test comment" };
+            var comment = new Comment { Content = "Test comment", Author = "Test author" };
 
             // Act & Assert
-            await Assert.ThrowsAsync<KeyNotFoundException>(() => commentService.AddCommentAsync(postId, comment));
+            await Assert.ThrowsAsync<KeyNotFoundException>(() => _commentService.AddCommentAsync(postId, comment));
         }
 
+        [Fact]
+        public async Task DeleteCommentAsync_ShouldReturnTrue_WhenCommentRemoved()
+        {
+            // Arrange            
+            var commentId = 1;
+            var newComment = new Comment { Content = "Some test comment", Author = "Test author" };
 
+            _dbContext.Comments.Add(newComment);
+            await _dbContext.SaveChangesAsync();
 
-        //[Fact]
-        //public async Task DeleteCommentAsync_Shoul_Delete_Comment_When_Exists()
-        //{
-        //    // Arrange
-        //    var (commentService, context) = GetCommentService();
-        //    var comment = new Comment { CommentId = 1, Content = "Test content", PostId = 1 };
+            // Act
+            var result = await _commentService.DeleteCommentAsync(commentId);
 
-        //    context.Comments.Add(comment);
-        //    await context.SaveChangesAsync();
+            // Assert
+            Assert.True(result);
+        }
 
-        //    // Act
-        //    await commentService.DeleteCommentAsync(comment.CommentId);
+        [Fact]
+        public async Task DeleteCommentAsync_ShouldReturnFalse_IfCommentDoesNotExist()
+        {
+            // Arrange            
+            var nonExistentCommentId = 999;            
 
-        //    // Assert
-        //    var detectedComment = await context.Comments.FindAsync(comment.CommentId);
-        //    Assert.Null(detectedComment);
-        //}
+            // Act
+            var result = await _commentService.DeleteCommentAsync(nonExistentCommentId);
 
-        //[Fact]
-        //public async Task DeleteCommentAsync_Should_NotFail_WhenComment_DoesNotExist()
-        //{
-        //    // Arrange
-        //    var (commentService, context) = GetCommentService();
-        //    var commentId = 999;
-
-        //    // Act
-        //    await commentService.DeleteCommentAsync(commentId);
-
-        //    // Assert
-        //    Assert.True(true); // Dummy assertion since the main goal is to ensure no exception is thrown
-        //}
-
-        //[Fact]
-        //public async Task EditCommentAsync_Should_Edit_Comment_ById()
-        //{
-        //    // Arrange
-        //    var (commentService, context) = GetCommentService();
-        //    var comment = new Comment { CommentId = 1, Content = "Test content", PostId = 1 };
-
-        //    context.Comments.Add(comment);
-        //    await context.SaveChangesAsync();
-
-        //    var editedComment = new Comment { CommentId = 1, Content = "Test content", PostId = 1 };
-
-        //    // Act
-        //    await commentService.EditCommentAsync(editedComment);
-
-        //    // Assert
-        //    var fetchedComment = await context.Comments.FindAsync(comment.CommentId);
-        //    Assert.NotNull(fetchedComment);
-        //    Assert.Equal(comment.Content, fetchedComment.Content);
-        //}
-
-        //[Fact]
-        //public async Task EditCommentAsync_Should_NotUpdate_NotExistentComment()
-        //{
-        //    // Arrange
-        //    var (commentService, context) = GetCommentService();
-
-        //    var nonExistentComment = new Comment { CommentId = 999, Content = "Non-existent content", PostId = 1 };
-
-        //    // Act
-        //    await commentService.EditCommentAsync(nonExistentComment);
-
-        //    // Assert
-        //    var fetchedComment = await context.Comments.FindAsync(nonExistentComment.CommentId);
-        //    Assert.Null(fetchedComment);
-        //}
+            // Assert
+            Assert.False(result);
+        }        
     }
 }
