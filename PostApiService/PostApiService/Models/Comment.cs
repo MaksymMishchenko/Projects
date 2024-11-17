7a410101f3d0ad2e3769ffd7dd0af6f481929b7d
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-11-17 20:28:43 +0200

    Add validation attributes to Comment model

--- PostApiService/PostApiService/Models/Comment.cs
+++ PostApiService/PostApiService/Models/Comment.cs
@@ -1,9 +1,17 @@
-﻿namespace PostApiService.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PostApiService.Models
 {
     public class Comment
     {
         public int CommentId { get; set; }
+
+        [Required(ErrorMessage = "Author is required.")]
+        [StringLength(500, ErrorMessage = "Author cannot exceed 25 characters.")]
         public string? Author { get; set; }
+
+        [Required(ErrorMessage = "Content is required.")]
+        [StringLength(500, ErrorMessage = "Content cannot exceed 500 characters.")]
         public string? Content { get; set; }
         public DateTime CreatedAt { get; set; }
 
