2c1a2da3d0ee54a59714fc8e4817bd38c204ab28
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-11-24 11:47:48 +0200

    Cover OnGetPosts and OnAddPost with integration tests

--- PostApiService/PostApiService.Tests/IntegrationTests/PostControllerTests.cs
+++ PostApiService/PostApiService.Tests/IntegrationTests/PostControllerTests.cs
@@ -3,17 +3,17 @@ using PostApiService.Tests.Helper;
 using System.Net.Http.Json;
 
 namespace PostApiService.Tests.IntegrationTests
-{
+{    
     public class PostControllerTests : IClassFixture<WebApplicationFactoryFixture>
     {
         public WebApplicationFactoryFixture _factory;
         public PostControllerTests(WebApplicationFactoryFixture factory)
         {
             _factory = factory;
-        }
+        }       
 
         [Fact]
-        public async Task OnGetPosts_WhenExecuteApi_ShouldReturnExpectedPosts()
+        public async Task OnGetPosts_WhenExecuteController_ShouldReturnTheExpectedPost()
         {
             // Arrange            
 
@@ -24,7 +24,34 @@ namespace PostApiService.Tests.IntegrationTests
             // Assert
             response.EnsureSuccessStatusCode();
             Assert.NotNull(result);
-            Assert.Equal(_factory.InitializePostData, result.Count);
-        }        
+            var list = DataFixture.GetPosts(_factory.InitializePostData);
+            Assert.Equal(list.Count, result.Count);
+            Assert.Equal(list.First().Title, result.First().Title);
+        }
+
+        [Fact]
+        public async Task OnAddPost_WhenExecuteController_ShouldStoreInDb()
+        {
+            // Arrange
+            var newPost = DataFixture.GetPost(true);
+
+            // Act            
+            var request = await _factory.HttpClient.PostAsync(HttpHelper.Urls.AddPost, HttpHelper.GetJsonHttpContent(newPost));
+            var response = await _factory.HttpClient.GetAsync($"{HttpHelper.Urls.GetPostById}/{_factory.InitializePostData + 1}");
+            var result = await response.Content.ReadFromJsonAsync<Post>();
+
+            // Assert
+            request.EnsureSuccessStatusCode();
+            response.EnsureSuccessStatusCode();
+
+            Assert.Equal(newPost.Title, result.Title);
+            Assert.Equal(newPost.Description, result.Description);
+            Assert.Equal(newPost.Slug, result.Slug);
+            Assert.Equal(newPost.ImageUrl, result.ImageUrl);
+            Assert.Equal(newPost.MetaTitle, result.MetaTitle);
+            Assert.Equal(newPost.MetaDescription, result.MetaDescription);
+            Assert.NotNull(result.Comments);
+            Assert.NotEmpty(result.Comments);
+        }
     }
 }
