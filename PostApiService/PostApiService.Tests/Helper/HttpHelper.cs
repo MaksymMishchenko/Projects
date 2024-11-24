0c1b50f3cf67808f1d29858f0dbf805040f34c40
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-11-24 11:47:48 +0200

    Cover OnGetPosts and OnAddPost with integration tests

--- PostApiService/PostApiService.Tests/Helper/HttpHelper.cs
+++ PostApiService/PostApiService.Tests/Helper/HttpHelper.cs
@@ -1,21 +1,20 @@
 ﻿using System.Text;
-using System.Text.Json;
 
 namespace PostApiService.Tests.Helper
 {
     internal class HttpHelper
     {
-        internal static StringContent GetJsonHttpContent(object item)
+        public static StringContent GetJsonHttpContent(object item)
         {
-            return new StringContent(JsonSerializer.Serialize(item), Encoding.UTF8, "application/json");
-            //return new StringContent(JsonConvert.SerializeObject(item), Encoding.UTF8, "application/json");
+            //return new StringContent(JsonSerializer.Serialize(item), Encoding.UTF8, "application/json");
+            return new StringContent(System.Text.Json.JsonSerializer.Serialize(item), Encoding.UTF8, "application/json");
         }
 
         internal static class Urls
         {
             public readonly static string GetAllPosts = "/api/Posts/GetAllPosts";
             public readonly static string GetPostById = "/api/Posts/GetPost";
-            public readonly static string AddPost = "/api/Posts/AddPostAsync";
+            public readonly static string AddPost = "/api/Posts/AddNewPost";
             public readonly static string EditPost = "/api/Posts/EditPostAsync";
             public readonly static string DeletePost = "/api/Posts/DeletePostAsync";
         }
