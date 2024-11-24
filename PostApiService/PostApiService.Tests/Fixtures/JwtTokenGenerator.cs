6c50f3c14d8121dcfb7c34bfc3711763b9c9ad20
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-11-24 14:37:34 +0200

    Cover AddPostAsync method with integratin test in case if user Unauthorized

---
+++ PostApiService/PostApiService.Tests/Fixtures/JwtTokenGenerator.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace PostApiService.Tests.Fixtures
+{
+    public static class JwtTokenGenerator
+    {
+        public static string GenerateJwtToken()
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("kYM3sxL49j4dz7AX7Sh5Np9yNX7Tb6v7kYM3sxL49j4dz7AX7Sh5Np9yNX7Tb6v7"));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+            new Claim(ClaimTypes.Name, "TestUser"),
+            new Claim(ClaimTypes.Role, "Admin")
+        };
+
+            var token = new JwtSecurityToken(
+                issuer: "yourIssuer",
+                audience: "yourAudience",
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
--- PostApiService/PostApiService.Tests/IntegrationTests/PostControllerTests.cs
+++ PostApiService/PostApiService.Tests/IntegrationTests/PostControllerTests.cs
@@ -1,19 +1,20 @@
 ﻿using PostApiService.Models;
 using PostApiService.Tests.Helper;
+using System.Net;
 using System.Net.Http.Json;
 
 namespace PostApiService.Tests.IntegrationTests
-{    
+{
     public class PostControllerTests : IClassFixture<WebApplicationFactoryFixture>
     {
         public WebApplicationFactoryFixture _factory;
         public PostControllerTests(WebApplicationFactoryFixture factory)
         {
             _factory = factory;
-        }       
+        }
 
         [Fact]
-        public async Task OnGetPosts_WhenExecuteController_ShouldReturnTheExpectedPost()
+        public async Task GetAllPosts_ShouldReturnAllPosts_WithoutToken()
         {
             // Arrange            
 
@@ -30,28 +31,20 @@ namespace PostApiService.Tests.IntegrationTests
         }
 
         [Fact]
-        public async Task OnAddPost_WhenExecuteController_ShouldStoreInDb()
+        public async Task AddPostWithoutToken_ReturnsUnauthorized()
         {
             // Arrange
-            var newPost = DataFixture.GetPost(true);
+            var post = DataFixture.GetPost(true);
+            var request = new HttpRequestMessage(HttpMethod.Post, HttpHelper.Urls.AddPost);
+            request.Content = HttpHelper.GetJsonHttpContent(post);
 
-            // Act            
-            var request = await _factory.HttpClient.PostAsync(HttpHelper.Urls.AddPost, HttpHelper.GetJsonHttpContent(newPost));
-            var response = await _factory.HttpClient.GetAsync($"{HttpHelper.Urls.GetPostById}/{_factory.InitializePostData + 1}");
-            var result = await response.Content.ReadFromJsonAsync<Post>();
+            // Act
+            var response = await _factory.HttpClient.SendAsync(request);
 
             // Assert
-            request.EnsureSuccessStatusCode();
-            response.EnsureSuccessStatusCode();
-
-            Assert.Equal(newPost.Title, result.Title);
-            Assert.Equal(newPost.Description, result.Description);
-            Assert.Equal(newPost.Slug, result.Slug);
-            Assert.Equal(newPost.ImageUrl, result.ImageUrl);
-            Assert.Equal(newPost.MetaTitle, result.MetaTitle);
-            Assert.Equal(newPost.MetaDescription, result.MetaDescription);
-            Assert.NotNull(result.Comments);
-            Assert.NotEmpty(result.Comments);
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
         }
+
+
     }
 }
